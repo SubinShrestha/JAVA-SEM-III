618bc0f56647a5e942eb0d3962fa06aa639cc26d
SubinShrestha <subin_0347@deerwalk.edu.np>
2015-01-19 14:19:04 +0545

    FILES OF SEM III [JAVA CLASSES]
    It contains the files and others from java class from SEM III

---
+++ DisplayAnimalPattern.java
@@ -0,0 +1,14 @@
+/**
+ * Created by user on 1/15/2015.
+ */
+public class DisplayAnimalPattern {
+    public static void main(String[]args){
+        System.out.println("          '__'");
+        System.out.println("          (©©)");
+        System.out.println("  /========\\/");
+        System.out.println(" / || %% ||");
+        System.out.println("*  ||----||");
+        System.out.println("   ¥¥    ¥¥");
+        System.out.println("   \"\"    \"\"");
+    }
+}
---
+++ Hello.java
@@ -0,0 +1,9 @@
+/**
+ * Created by user on 1/12/2015.
+ */
+public class Hello {
+    public static void main(String[]args){
+
+        System.out.println("!!!Hello,world!!!");
+    }
+}
